478a95c1f53f64842350478294226e9dc260bd0a
Rick <rickjohannes@hotmail.nl>
2020-05-13 16:19:38 +0200

    Klasse Artikel gemaakt

---
+++ src/KantineSimulatieBasis/src/main/java/Artikel.java
@@ -0,0 +1,31 @@
+package KantineSimulatieBasis.src.main.java;
+
+public class Artikel {
+    private String naam;
+    private int prijs;
+
+    public Artikel(String naam, int prijs) {
+        this.naam = naam;
+        this.prijs = prijs;
+    }
+
+    public Artikel() {
+
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public int getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(int prijs) {
+        this.prijs = prijs;
+    }
+}
