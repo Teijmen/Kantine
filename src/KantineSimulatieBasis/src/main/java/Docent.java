9e1cc110d409658bbeb76d6e27c03501767a889e
LarsBosker <larsbosker@gmail.com>
2020-06-02 15:01:11 +0200

    Week 3 t/m opdr 4

---
+++ src/KantineSimulatieBasis/src/main/java/Docent.java
@@ -0,0 +1,33 @@
+package KantineSimulatieBasis.src.main.java;
+
+public class Docent extends Persoon {
+    private String letterCode;
+    private String afdeling;
+
+    public Docent(int bsn, String voornaam, String achternaam, Datum geboortedatum, char geslacht, String letterCode, String afdeling) {
+        super(bsn, voornaam, achternaam, geboortedatum, geslacht);
+        this.letterCode = letterCode;
+        this.afdeling = afdeling;
+    }
+
+    public Docent() {
+
+
+    }
+
+    public String getLetterCode() {
+        return letterCode;
+    }
+
+    public void setLetterCode(String letterCode) {
+        this.letterCode = letterCode;
+    }
+
+    public String getAfdeling() {
+        return afdeling;
+    }
+
+    public void setAfdeling(String afdeling) {
+        this.afdeling = afdeling;
+    }
+}
