ff853ef3b7296cac349c398e390fd445c865fbe8
Rick <rickjohannes@hotmail.nl>
2020-06-09 13:22:46 +0200

    exception class

---
+++ src/KantineSimulatieBasis/src/main/java/TeWeinigGeldException.java
@@ -0,0 +1,14 @@
+package KantineSimulatieBasis.src.main.java;
+
+public class TeWeinigGeldException extends Exception{
+
+    public TeWeinigGeldException(){}
+
+    public TeWeinigGeldException(Exception e){
+        super(e);
+    }
+
+    public TeWeinigGeldException(String message){
+        super(message);
+    } //comment
+}
